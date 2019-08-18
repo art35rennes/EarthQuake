008999d159c1599a7e0747e2128b3bdcf1619d02
Arthur Sicard <arthur2rennes@hotmail.fr>
2019-08-18 12:55:09 -0400

    Template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,12 +68,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Accueil</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Connexion</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">S'inscrire</a>
                         @endif
                     @endauth
                 </div>
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Earthquake
                 </div>
 
                 <div class="links">
